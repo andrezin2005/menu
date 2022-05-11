5af86fdb0720f0f44d15f655a8283d2b530664d9
andrezin2005 <105053957+andrezin2005@users.noreply.github.com>
2022-05-11 19:33:01 -0300

    Update mobile-navbar.js

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -1,40 +1,61 @@
-class MobileNavBar {
+function onScroll() {
+    if(scrollY > 0) {
+        navigation.classList.add('scroll') 
+    } else {
+        navigation.classList.remove("scroll")
+    }
+}
+class MobileNavbar {
     constructor(mobileMenu, navList, navLinks) {
         this.mobileMenu = document.querySelector(mobileMenu);
         this.navList = document.querySelector(navList);
         this.navLinks = document.querySelectorAll(navLinks);
         this.activeClass = "active";
+        this.activeMenu = "menu-expanded";
 
         this.handleClick = this.handleClick.bind(this);
     }
+
     animateLinks() {
         this.navLinks.forEach((link, index) => {
-            link.style.animation
-                ? (link.style.animation = "")
-                : (link.style.animation = `navLinkFade 0.5s ease forwards ${
-                    index / 7 + 0.3
-                  }s`);
+        link.style.animation
+            ? (link.style.animation = "")
+            : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+                index / 7 + 0.3
+            }s`);
         });
     }
+
     handleClick() {
         this.navList.classList.toggle(this.activeClass);
         this.mobileMenu.classList.toggle(this.activeClass);
+        this.body.classList.toggle(this.activeMenu);
+
         this.animateLinks();
     }
+
     addClickEvent() {
-        this.mobileMenu.addEventListener("click",this.handleClick);
+        this.mobileMenu.addEventListener("click", this.handleClick);
     }
+
     init() {
         if (this.mobileMenu) {
-            this.addClickEvent();
+        this.addClickEvent();
         }
         return this;
     }
-}
+    }
 
-const mobileNavbar = new MobileNavBar(
+const mobileNavbar = new MobileNavbar(
     ".mobile-menu",
     ".nav-list",
     ".nav-list li",
-);
+    );
 mobileNavbar.init();
+
+function openMenu() {
+    document.body.classList.add("menu-expanded")
+}
+function closeMenu() {
+    document.body.classList.remove("menu-expanded")
+}
