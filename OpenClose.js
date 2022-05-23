4b32d49a64b6ac6079465a028de83252ca1ecf78
andrezin2005 <105053957+andrezin2005@users.noreply.github.com>
2022-05-23 19:17:21 -0300

    Add files via upload

---
+++ OpenClose.js
@@ -0,0 +1,23 @@
+//Adiciona e remove a classe da seção
+//Matéria de Língua Portuguesa=====================================================================
+function OpenCloseLP() {
+    this.sectionLP.classList.toggle("OpenClose")
+}
+
+//Matéria de Matemática============================================================================
+//Arranjo
+function OpenCloseAJ() {
+    this.sectionAJ.classList.toggle("OpenClose")
+}
+//Combinações
+function OpenCloseCB() {
+    this.sectionCB.classList.toggle("OpenClose")
+}
+
+
+//Matéria de Química===============================================================================
+function OpenCloseQM() {
+    this.sectionKW.classList.toggle("OpenClose")
+}
+
+
