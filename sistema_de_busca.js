5657abf0fc3b7d9198772407584b4f1899a2ddd1
andrezin2005 <105053957+andrezin2005@users.noreply.github.com>
2022-05-30 19:16:49 -0300

    Update sistema_de_busca.js

--- sistema_de_busca.js
+++ sistema_de_busca.js
@@ -19,3 +19,68 @@ function busca(input_field,div){
         }
     }
 }
+function busca(input_field,div){
+    input_field.onkeyup=function(e){
+        for(di of div.children){
+            input = document.getElementById("SearchMT");
+            r  = new RegExp(input.value.toLowerCase(),"g")
+            //if(di.getAttribute("nome").toLowerCase().match(r) != null)
+            if(di.getAttribute("nome").match(r) != null)
+                di.style.removeProperty('display')
+            else
+                di.style.display = "none"
+        }
+    }
+}
+function busca(input_field,div){
+    input_field.onkeyup=function(e){
+        for(di of div.children){
+            input = document.getElementById("SearchEPL");
+            r  = new RegExp(input.value.toLowerCase(),"g")
+            //if(di.getAttribute("nome").toLowerCase().match(r) != null)
+            if(di.getAttribute("nome").match(r) != null)
+                di.style.removeProperty('display')
+            else
+                di.style.display = "none"
+        }
+    }
+}
+function busca(input_field,div){
+    input_field.onkeyup=function(e){
+        for(di of div.children){
+            input = document.getElementById("SearchFS");
+            r  = new RegExp(input.value.toLowerCase(),"g")
+            //if(di.getAttribute("nome").toLowerCase().match(r) != null)
+            if(di.getAttribute("nome").match(r) != null)
+                di.style.removeProperty('display')
+            else
+                di.style.display = "none"
+        }
+    }
+}
+function busca(input_field,div){
+    input_field.onkeyup=function(e){
+        for(di of div.children){
+            input = document.getElementById("SearchQM");
+            r  = new RegExp(input.value.toLowerCase(),"g")
+            //if(di.getAttribute("nome").toLowerCase().match(r) != null)
+            if(di.getAttribute("nome").match(r) != null)
+                di.style.removeProperty('display')
+            else
+                di.style.display = "none"
+        }
+    }
+}
+function busca(input_field,div){
+    input_field.onkeyup=function(e){
+        for(di of div.children){
+            input = document.getElementById("SearchPT");
+            r  = new RegExp(input.value.toLowerCase(),"g")
+            //if(di.getAttribute("nome").toLowerCase().match(r) != null)
+            if(di.getAttribute("nome").match(r) != null)
+                di.style.removeProperty('display')
+            else
+                di.style.display = "none"
+        }
+    }
+}
